98c1d3474ccea26983f0b46dc5816ffe662014db
Maxime Bodin <maxime.bodin@mailo.com>
2025-06-06 00:17:00 +0200

    Handling credentials in config.py.

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'), overrid
 
 class Config:
     # Scraper credentials.
-    USERNAME = os.getenv('SCRAPER_USERNAME', '')
-    PASSWORD = os.getenv('SCRAPER_PASSWORD', '')
+    USERNAME = os.getenv('SCRAPER_USERNAME', 'your_username_here')
+    PASSWORD = os.getenv('SCRAPER_PASSWORD', 'your_password_here')
     
     # Throw error if credentials are default placeholders.
     if USERNAME in ('', 'your_username_here') or PASSWORD in ('', 'your_password_here'):
@@ -23,3 +23,16 @@ class Config:
     
     # Health check.
     HEALTH_CHECK_PORT = int(os.getenv('HEALTH_CHECK_PORT', '8080'))
+
+    # API settings
+    DEV_API_URL = os.getenv('DEV_API_URL', 'http://127.0.0.1:3000')
+    PROD_API_URL = os.getenv('PROD_API_URL', 'https://transat.destimt.fr')
+    API_EMAIL = os.getenv('API_EMAIL', 'your_email_here')
+    API_PASSWORD = os.getenv('API_PASSWORD', 'your_password_here')
+    USER_EMAIL = os.getenv('USER_EMAIL', 'your_email_here')
+
+    # Throw error if API credentials are default placeholders.
+    if API_EMAIL in ('', 'your_email_here') or API_PASSWORD in ('', 'your_password_here'):
+        raise ValueError("API_EMAIL and API_PASSWORD must be set in your .env file and not use default values!")
+    if USER_EMAIL in ('', 'your_email_here'):
+        raise ValueError("USER_EMAIL must be set in your .env file and not use default values!")
